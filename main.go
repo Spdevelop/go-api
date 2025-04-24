2a6e1134b2d5132025ce625c723a6e8051933795
Supat Baingam <wheel_2538@hotmail.com>
2025-04-24 13:50:18 +0700

    first go deploy

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Employee struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name       string             `json:"name"`
+	Position   string             `json:"position"`
+	Department string             `json:"department"`
+}
+
+var collection *mongo.Collection
+
+func connectDB() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://<user>:<pass>@cluster.mongodb.net/mydb"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("mydb").Collection("employees")
+}
+
+func getEmployees(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	var employees []Employee
+	if err = cursor.All(ctx, &employees); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, employees)
+}
+
+func createEmployee(c *gin.Context) {
+	var emp Employee
+	if err := c.ShouldBindJSON(&emp); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := collection.InsertOne(ctx, emp)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	emp.ID = res.InsertedID.(primitive.ObjectID)
+	c.JSON(http.StatusOK, emp)
+}
+
+func deleteEmployee(c *gin.Context) {
+	id := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := collection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if res.DeletedCount == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Deleted"})
+}
+
+func main() {
+	connectDB()
+
+	r := gin.Default()
+	r.GET("/employees", getEmployees)
+	r.POST("/employees", createEmployee)
+	r.DELETE("/employees/:id", deleteEmployee)
+
+	port := "3000"
+	fmt.Println("Server running at http://localhost:" + port)
+	r.Run(":" + port)
+}
